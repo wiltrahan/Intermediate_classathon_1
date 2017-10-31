a0b7d063033b2aa293b9ca6d789e1fc04f795ed4
Wil Trahan <wiltrahan@intracitygeeks.org>
2017-10-31 00:48:35 +0000

    complete sumPrimes

--- sumPrimes.c
+++ sumPrimes.c
@@ -1,87 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
-
-//THIS IS A MESS!!!!!
-
-
 int sumPrimes(int num);
-int getPrimes(int max);
+bool isPrime(int number);
 
 
 int main(void) {
     printf("%i\n", sumPrimes(10));
 }
 
-int sumPrimes(int num) {
-    int res = 0;
-
-    // Add the primes
-  int primes[10];
-  getPrimes(num);
-  for (int i = 0; i < sizeof(primes); i++) {
-    res += primes[i];
-  }
-  return res;
-}
-
-int getPrimes(int max) {
-    int sieve[10];
-    int primes[10];
-    for(int i = 2; i <= max; i++) {
-        if(!sieve[i]) {
-            primes[i] = i;
-            for (int j = i << 1; j <= max; j += i) {
-                sieve[j] = true;
-            }
-        }
-
-        return primes[i];
+int sumPrimes(num) {
+    if (num == 1) {
+        return 0;
     }
 
-    return 0;
+    if(isPrime(num) == false) {
+        return sumPrimes(num - 1);
+    }
+
+    if(isPrime(num) == false){
+        return sumPrimes(num - 1);
+    }
+    if(isPrime(num) == true){
+        return num + sumPrimes(num - 1);
+    }
+    return num;
 }
 
+bool isPrime(int number) {
+    for(int i = 2; i <= number; i++) {
+        if(number % i == 0 && number != i) {
+            return false;
+        }
+    }
+    return true;
 
+}
 
-
-
-
-
-
-
-
-
-
-
-// function sumPrimes(num) {
-//   var res = 0;
-
-//   // Function to get the primes up to max in an array
-//   function getPrimes(max) {
-//     var sieve = [];
-//     var i;
-//     var j;
-//     var primes = [];
-//     for (i = 2; i <= max; ++i) {
-//       if (!sieve[i]) {
-//         // i has not been marked -- it is prime
-//         primes.push(i);
-//         for (j = i << 1; j <= max; j += i) {
-//           sieve[j] = true;
-//         }
-//       }
-//     }
-
-//     return primes;
-//   }
-
-//   // Add the primes
-//   var primes = getPrimes(num);
-//   for (var p = 0; p < primes.length; p++) {
-//     res += primes[p];
-//   }
-
-//   return res;
-// }
